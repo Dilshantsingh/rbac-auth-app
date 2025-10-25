23b7125c8069c1705cac41d727e2315bfd796b97
Dilshantsingh <Dilshant2004@gmail.com>
2025-10-25 19:08:42 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import protectedRoutes from "./routes/protectedRoutes.js";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.use("/", authRoutes);
+app.use("/", protectedRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
